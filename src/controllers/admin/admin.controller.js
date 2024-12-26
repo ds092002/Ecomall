20c45d5666e86bbb74455e83c0ffe8b767dae47c
Dhaval Solanki <dhavalas24@gamil.com>
2024-12-26 11:43:39 +0530

    26 Dec

--- src/controllers/admin/admin.controller.js
+++ src/controllers/admin/admin.controller.js
@@ -5,9 +5,9 @@ const jwt = require('jsonwebtoken');
 
 exports.registerAdmin = async (req, res) => {
     try {
-        let user = await adminService.getUser({ email: req.body.email});
-        console.log(user);
-        if (user) {
+        let admin = await adminService.getUser({ email: req.body.email});
+        console.log(admin);
+        if (admin) {
             return res.status(400).json({message: `User is already registered....👍🏻`})
         };
         if (req.file) {
@@ -18,10 +18,61 @@ exports.registerAdmin = async (req, res) => {
         console.log(hashPassword);
         admin = await adminService.addNewUser({
             ...req.body,
-            password: hashPassword
+            password: hashPassword,
+            isAdmin: true
         });
         res.status(201).json({ admin : admin})
     } catch (error) {
-        
+        console.log(error.message);
+        res.status(500).json({message: `Internal Server Error.${console.error()}`})
+    }
+}
+
+exports.loginAdmin = async (req, res) => {
+    try {
+        let admin = await adminService.getUser({email: req.body.email, isDelete: false});
+        console.log(admin);
+        if (!admin) {
+            return res.status(404).json({message: `User not found or deleted....Please Check your email address`})
+        }
+        let checkPassword = await bcryptjs.compare(req.body.password, admin.password);
+        if (!checkPassword) {
+            return res.status(401).json({message: `Invalid Password....Please Try Again`});
+        }
+        let token = jwt.sign({ adminId: admin._id},'Admin');
+        console.log(token);
+         res.status(200).json({token, message: `Login SuccesFully........`});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: `Internal Server Error.${console.error()}`})
+    }
+}
+
+exports.getAllAdmin = async (req, res) => {
+    try {
+        let admin = await adminService.getAllUsers({isDelete: false, isAdmin: true});
+        console.log(admin);
+        if (!admin) {
+            return res.status(404).json({message: `Admin data not found please try again.....!`})
+        }
+        res.status(200).json({admin})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: `Internal Server Error.${console.error()}`})
+    }
+}
+
+exports.getAdmin = async (req, res) => {
+    try {
+        let admin = await adminService.getUserById(req.query.adminId);
+        console.log(admin);
+        if (!admin) {
+            return res.status(404).json({message:`Admin data not found please try again.....!`})
+        }
+        admin = await adminService.updateUser(admin._id,{...req.body});
+        res.status(201).json({admin, message:`Admin updated successfully`})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: `Internal Server Error.${console.error()}`})        
     }
 }
